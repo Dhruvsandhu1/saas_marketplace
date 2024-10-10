549b253e065326b29d9fd516b593cb33c41262b1
Dhruvsandhu1 <152141078+Dhruvsandhu1@users.noreply.github.com>
2024-10-11 00:59:37 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -7,19 +7,22 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 # Set page title and icon
-import streamlit as st
+import json
+import time
 st.set_page_config(page_title="SaaS Marketplace", page_icon=":chart_with_upwards_trend:", layout="wide")
 
 # Sidebar
 st.sidebar.title("Navigation")
 st.sidebar.markdown("Choose your page:")
-page = st.sidebar.selectbox("", ["Home", "About", "Contact"])
+page = st.sidebar.selectbox("", ["Home", "About Us", "Contact","Customer Profile"])
 
 # Main header
 # st.title("SaaS Marketplace")
 # st.subheader("We provide information about the latest software that are in the industry and helps you meet your software requirements")
 
 def remove_html_tags(html_content):
+    # $ is reserved for latex expression in streamlit so replace it
+    html_content = html_content.replace('$', r'\$')
     soup = BeautifulSoup(html_content, 'html.parser')
     flag=0
     for element in soup:
@@ -149,7 +152,7 @@ if page == "Home":
 
     
 # About Page
-elif page == "About":
+elif page == "About Us":
     st.markdown('<h1>About <span class="jazzee-title">Jazzee</span> Assist</h1>', unsafe_allow_html=True)
 
     st.write(
@@ -233,3 +236,133 @@ elif page == "Contact":
                 st.success("Your message has been sent successfully!")
             else:
                 st.error("There was an error sending your message. Please try again.")
+
+elif page=="Customer Profile":
+    # Define the API endpoint and authorization token
+    API_URL = "https://app.reviewflowz.com/api/v2/accounts/1850/listings"
+    LISTINGS_API_URL = "https://app.reviewflowz.com/api/v2/accounts/1850/listings?count=10000"
+    AUTH_TOKEN = "eNSjxYh8fPKzUqAoCELWx5xt"  # Replace with your actual token
+    # Streamlit app setup
+    st.markdown('<h1><span class="jazzee-title">Jazzee</span> Software Reviews</h1>', unsafe_allow_html=True)
+
+    # Input fields for the user
+    software_name = st.text_input("Enter the software name:")
+    platform = st.selectbox("Select the platform name:", ["G2", "Capterra"])
+    base_url = "https://www.g2.com/products/{software}/reviews"
+    exp_site_url = base_url.format(software=software_name)
+    site_url= st.text_input("Enter the url",value=f"{exp_site_url}")
+
+
+    # Function to make API request and extract the ID
+    def create_listing(software_name, platform, site_url):
+        headers = {
+            "accept": "*/*",
+            "Authorization": f"Bearer {AUTH_TOKEN}",
+            "Content-Type": "application/json",
+        }
+        data = {
+            "profile_name": software_name,
+            "url": site_url,
+            "platform": platform,
+        }
+
+        response = requests.post(API_URL, headers=headers, data=json.dumps(data))
+        
+        if response.status_code == 200:
+            # Assuming the response contains JSON data with a 'profile_id' field
+            response_data = response.json()  # Get the JSON response
+            profile_id = response_data.get("profile_id", None)  # Extract 'profile_id'
+            return profile_id, response.status_code
+        else:
+            return None, response.status_code
+
+
+    # Function to get all listings
+    def get_listings(auth_token):
+        headers = { 
+            "accept": "application/json",
+            "Authorization": f"Bearer {auth_token}",
+        }
+
+        response = requests.get(LISTINGS_API_URL, headers=headers)
+        
+        if response.status_code == 200:
+            return response.json()  # Return the JSON response
+        else:
+            st.error(f"Failed to fetch listings. Status Code: {response.status_code}")
+            return None
+
+    # Function to get account id    
+    def get_account_id(listing,have_id):
+        for i in range(len(listing['data'])):
+            if listing['data'][i]['platform']==platform and listing['data'][i]['profile_name'].lower()==software_name.lower():
+                have_id=1
+                # st.write(listing['data'][i]['id'])
+                return have_id,listing['data'][i]['id']
+        return 0,0
+        
+    # Function to fetch the review
+    def fetch_review(platform, token_number):
+        api_url = "https://app.reviewflowz.com/api/v2/accounts/1850/reviews"
+        headers = {
+            "accept": "application/json",
+            "Authorization": "Bearer eNSjxYh8fPKzUqAoCELWx5xt"  
+        }
+        
+        params = {
+            "platform": platform,
+            "review_listing_ids[]": token_number
+        }
+        
+        try:
+            response = requests.get(api_url, headers=headers, params=params)
+            response.raise_for_status()  # Raise an error for bad responses (4xx or 5xx)
+            return response.json()  # Return the review data in JSON format
+        except requests.exceptions.RequestException as e:
+            return {"error": str(e)}
+
+    # Submit button    
+    if st.button("Fetch Reviews"):
+        done=0
+        while True:
+            listing=get_listings(AUTH_TOKEN)
+            account_id=0
+            have_id=0
+            #to display all the listing 
+            # st.write(listing)
+
+            have_id,account_id=get_account_id(listing,have_id)
+            if software_name and platform and site_url and have_id!=1:
+                # st.write("I am running")
+                profile_id, status_code = create_listing(software_name, platform, site_url)
+                if status_code == 200 and profile_id:
+                    st.success(f"Listing created successfully for {software_name} on {platform}.")
+                    dashboard_url = f"https://app.reviewflowz.com/review_profiles/{profile_id}"
+                    st.write(f"Visit the dashboard: {dashboard_url}")
+                    new_listing=get_listings(AUTH_TOKEN)
+                    have_id,account_id=get_account_id(new_listing,have_id)
+
+                    st.write(f"Your id is {account_id}")
+                # elif have_id!=1:
+                #     st.error(f"Failed to create listing due to incorrect configurations . Status Code: {status_code}")
+            elif have_id!=0:
+                have_id=have_id
+            else:
+                st.error("Please fill out all fields.")
+            new_listing=get_listings(AUTH_TOKEN)
+            # st.write(new_listing)
+            have_id,account_id=get_account_id(new_listing,have_id)
+            # st.write(account_id)
+            review_data = fetch_review(platform, account_id)
+            if "error" in review_data:
+                st.error(f"Failed to fetch the review: {review_data['error']}")
+            elif review_data['data']!=[]:
+                review=review_data
+                # st.write(review_data)
+                for i in range(len(review['data'])):
+                    st.markdown(f"**<span style='font-size: 24px;'>{review['data'][i]['title']}</span>**",unsafe_allow_html=True)
+                    st.markdown(f"**<span style='font-size: 18px;'>Overall :</span>** {review['data'][i]['overall']}",unsafe_allow_html=True)
+                    st.markdown(f"**<span style='font-size: 18px;'>Pros :</span>** {review['data'][i]['pros']}",unsafe_allow_html=True)
+                    st.markdown(f"**<span style='font-size: 18px;'>Cons :</span>** {review['data'][i]['cons']}",unsafe_allow_html=True)
+                    st.markdown("__________________________")
+                break
